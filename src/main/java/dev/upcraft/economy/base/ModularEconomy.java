083f74d551fc815178df3db03f89946211f65a6d
Up <upcraftedlp@gmail.com>
2023-12-16 14:40:26 +0100

    base mod skeleton

---
+++ src/main/java/dev/upcraft/economy/base/ModularEconomy.java
@@ -0,0 +1,9 @@
+package dev.upcraft.economy.base;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(ModularEconomy.MODID)
+public class ModularEconomy {
+
+    public static final String MODID = "modular_economy";
+}
